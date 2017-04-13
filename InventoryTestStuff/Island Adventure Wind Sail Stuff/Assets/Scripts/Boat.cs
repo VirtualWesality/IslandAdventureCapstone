e68a286bf02671abe3f1673609d1de742f16b596
helo <carver0428@hotmail.com>
2017-04-13 00:40:21 -0700

    Inventory Start
    This is the start of an inventory system. Wasn't to bad work just kept
    getting in the way. Let me know what you think

---
+++ InventoryTestStuff/Island Adventure Wind Sail Stuff/Assets/Scripts/Boat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// dont touch Tim Allen
+/// </summary>
+public class Boat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
