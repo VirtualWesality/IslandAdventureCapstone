3239d27176afa4d8c6c177bf98732ece3fc150c7
navanafti <Student@1913216-C00000.lanecc.edu>
2017-04-25 13:51:50 -0700

    Added My project to my own branch

---
+++ WesleysMostRecentProject/StartMenu/IA9_Title_Screen/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform canvas;
+    public Transform Player;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+        }
+	}
+
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            Player.GetComponent<ThirdPersonUserControl>().enabled = false;
+        }     
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ContinuePress()
+    {
+        canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+        Player.GetComponent<ThirdPersonUserControl>().enabled = true;
+    }
+}
