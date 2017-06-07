685dab40f689f0cbc7d91bf9359fcdfc64c98604
VirtualWesality <westheman81@gmail.com>
2017-06-07 04:15:35 -0700

    Added Health, hunger, and thirst
    This opens a whole nother can a worms boys

---
+++ WesleysProject/IA9_Title_Screen/Assets/Scripts/PlayerVitals.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerVitals : MonoBehaviour {
+
+    public Slider healthSlider, thirstSlider, hungerSlider;
+    public int maxHealth, healthFallRate;
+    public int maxThirst, thirstFallRate;
+    public int maxHunger, hungerFallRate;
+
+    private void Start()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+
+        thirstSlider.maxValue = maxThirst;
+        thirstSlider.value = maxThirst;
+
+        hungerSlider.maxValue = maxHunger;
+        hungerSlider.value = maxHunger;
+    }
+
+    private void Update()
+    {
+        //Health Controller
+        if (hungerSlider.value <= 0 && (thirstSlider.value <= 0))
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate * 2;
+        }
+        else if (hungerSlider.value <= 0 || thirstSlider.value <= 0)
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate;
+        }
+
+        if (healthSlider.value <= 0)
+        {
+            CharacterDeath();
+        }
+
+        //Hunger Controller
+        if (hungerSlider.value >= 0)
+        {
+            hungerSlider.value -= Time.deltaTime / hungerFallRate;
+        }
+        else if (hungerSlider.value <= 0)
+        {
+            hungerSlider.value = 0;
+        }
+        else if (hungerSlider.value >= maxHunger)
+        {
+            hungerSlider.value = maxHunger;
+        }
+
+        //Thirst Controller
+        if (thirstSlider.value >= 0)
+        {
+            thirstSlider.value -= Time.deltaTime / thirstFallRate;
+        }
+        else if (thirstSlider.value <= 0)
+        {
+            thirstSlider.value = 0;
+        }
+        else if (thirstSlider.value >= maxThirst)
+        {
+            thirstSlider.value = maxThirst;
+        }
+    }
+
+    void CharacterDeath()
+    {
+        //Do something death related
+    }
+
+}
