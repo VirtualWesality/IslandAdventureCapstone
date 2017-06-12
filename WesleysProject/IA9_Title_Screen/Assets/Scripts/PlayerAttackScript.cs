c75569b9361b19e1898b115cb9a8e87e8e06c7ea
Ryan Dietrich <ryan.s.dietrich@outlook.com>
2017-06-11 22:52:06 -0700

    Meta

---
+++ WesleysProject/IA9_Title_Screen/Assets/Scripts/PlayerAttackScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerAttackScript : MonoBehaviour {
+    public Animator anim;
+    
+	// Use this for initialization
+	void Start ()
+    {
+       anim = GetComponent<Animator>();//Get Animation component, store in anim
+    }
+	
+	// Update is called once per frame
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) //If player presses F
+        {
+            anim.SetBool("SwordSwing", true);
+            
+            anim.SetBool("SwordSwing", false);
+            //Attack();//Call Attack
+        }
+		
+	}
+
+    private void Attack() //Called on click
+    {        
+       //  anim.Play(anim.clip.name);//Play the attack animation
+
+    }
+}
